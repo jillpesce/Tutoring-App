08c7c4a26e8788e3ec3facdc27959a2121cb65c2
caromurp <57685459+caromurp@users.noreply.github.com>
2020-04-15 16:07:24 -0400

    user can see score progress, updates on style

--- routes/scores-routes.js
+++ routes/scores-routes.js
@@ -1,5 +1,6 @@
 var router = require('express').Router();
 var User = require('../models/User');
+var Chart = require('chart.js');
 
 var authCheck = (req, res, next) => {
     if(!req.user) {
@@ -18,14 +19,33 @@ router.get('/', authCheck, (req, res) => {
 
 //Post new score
 router.post('/save', (req, res) => {
-    req.user.scores.push(req.body.inputScore);
+    var date = new Date(); 
+    var timestamp = date.getTime();
+    var course = req.body.course;
+    scoreTuple = [timestamp, req.body.inputScore, course];
+    req.user.scores.push(scoreTuple);
     console.log(req.user.scores);
     var newValues = { $set: {
         scores: req.user.scores
     }};
+
     User.updateOne({email: req.user.email}, newValues).then(() => {
-        console.log('New scores = ' + req.user.scores);
-        res.redirect('/scores');
+        console.log('New scores = ' + JSON.stringify(req.user.scores));
+
+        var data = [];
+        var count = 1;
+        for (var i = 0; i < req.user.scores.length; i++) {
+            if (req.user.scores[i][2] == course) {
+                data.push({
+                    "x": req.user.scores[i][1],
+                    "y": count
+                });
+                count++;
+            }
+        }
+
+        res.render('progress', { course: course, data: data, user: req.user });
     });
 });
+
 module.exports = router;
