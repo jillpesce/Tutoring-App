9f1d38593ebf549d9ffd45f3f4d9cd6774170ee0
snie11 <selinanie11@gmail.com>
2020-04-15 22:27:01 -0500

    finished dynamic appts

--- android/app/src/main/java/edu/upenn/cis350/cis350_finalproject/AppointmentsTutorFragment.java
+++ android/app/src/main/java/edu/upenn/cis350/cis350_finalproject/AppointmentsTutorFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -40,7 +41,11 @@ public class AppointmentsTutorFragment extends Fragment implements OnItemClickLi
 
         RemoteDataSource ds = new RemoteDataSource();
         tutorAppts = ds.getTutorAppointments(user.getEmail());
-        Collections.sort(tutorAppts);
+        if (tutorAppts != null) {
+            Collections.sort(tutorAppts);
+        } else {
+            tutorAppts = new ArrayList<Appointment>();
+        }
         return view;
     }
 
